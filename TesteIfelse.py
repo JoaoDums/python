71e4e314d2c03d21962faaedaaa5874fea9f3ff4
João Pedro Dums <103467962+JoaoDums@users.noreply.github.com>
2026-04-14 09:32:18 -0300

    Add files via upload

---
+++ TesteIfelse.py
@@ -0,0 +1,8 @@
+idade = int(input("quantos anos você tem? "))
+
+if idade < 18 :
+    print(f"você é menor de idade")
+    
+else  :
+    print(f"você é maior de idade")
+     
---
+++ nome.py
@@ -0,0 +1,4 @@
+nome = input("qual o seu nome? ")
+idade = int(input("quantos anos você tem? "))
+
+print(f'seu nome é{nome} e vocÊ tem{idade} anos')
